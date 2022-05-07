4ed5058ee43fecb7678478904567689dddb99490
Thiago Petrimperni <thiago.srpt@gmail.com>
2022-05-07 13:31:39 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ window.addEventListener('load', ()=> {
             console.log(long);
             console.log(position)
 
-            const api = `http://api.weatherapi.com/v1/current.json?key=c2ef9cc9ff124396863165707220705&q=${lat},${long}&aqi=no`;
+            const api = `https://api.weatherapi.com/v1/current.json?key=c2ef9cc9ff124396863165707220705&q=${lat},${long}&aqi=no`;
 
             //must be place here bc the const api is not available outise of this bracket scope.
             fetch(api)
